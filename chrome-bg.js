54f80764806c987e6cc1f65654bd70078f216883
Shrikant Sharat <shrikantsharat.k@gmail.com>
2014-04-04 00:03:58 +0530

    Solarized scrollbars.

--- chrome-bg.js
+++ chrome-bg.js
@@ -2,8 +2,8 @@
 /*global chrome */
 chrome.app.runtime.onLaunched.addListener(function () {
     var w = 520, h = 600,
-        left = screen.width/2 - w/2,
-        top = screen.height/2 - h/2 - 12;
+        left = screen.width - w,
+        top = screen.height/2 - h/2;
     chrome.app.window.create('index.html', {
         width: w,
         height: h,

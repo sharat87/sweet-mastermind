637f3fc1a18998b8007b298650706e9aba1bbea7
Shrikant Sharat <shrikantsharat.k@gmail.com>
2014-05-17 15:52:00 +0530

    Slight window padding to the right.

--- chrome-bg.js
+++ chrome-bg.js
@@ -2,7 +2,7 @@
 /*global chrome */
 chrome.app.runtime.onLaunched.addListener(function () {
     var w = 520, h = 600,
-        left = screen.width - w,
+        left = screen.width - w - 12,
         top = screen.height/2 - h/2;
     chrome.app.window.create('index.html', {
         width: w,
